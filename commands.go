adba0319bd891c3a6d35469fc91047e85435e69f
Daniel Rapp <daniel.rapp@live.se>
2018-08-29 01:21:36 +0200

    just some stuff

--- commands.go
+++ commands.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -21,7 +22,15 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if sString[0] == "!afk" {
 		s.ChannelMessageSend(m.ChannelID, "slacker! but its ok")
 		if reDate.MatchString(sString[1]) {
+			layout := "2006-01-02T15:04:05.000Z"
 			fmt.Println("theres a date!?")
+			t, err := time.Parse(layout, sString[1]+"T19:00:00.000Z")
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			s.ChannelMessageSend(m.ChannelID, "Your attendance for "+sString[1]+" has been confirmed")
+			fmt.Println(t)
 		}
 	}
 
